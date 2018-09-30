dea9cdc11d311f27e1fb86dc5ac1e85c6b003ebb
rajeevkr <rajeev.kumar@vuclip.com>
2018-09-30 15:07:57 +0530

    First commit

---
+++ src/main/java/com/vuclip/naive/bayes/analyser/AnalyserApplication.java
@@ -0,0 +1,12 @@
+package com.vuclip.naive.bayes.analyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyserApplication.class, args);
+	}
+}
---
+++ src/test/java/com/vuclip/naive/bayes/analyser/AnalyserApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vuclip.naive.bayes.analyser;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AnalyserApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
